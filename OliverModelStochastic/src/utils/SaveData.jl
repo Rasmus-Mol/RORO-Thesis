b4b7c70a79daca4c064552a65a30d2eaeb1b72e6
mollergøj <s194364@student.dtu.dk>
2025-03-27 09:48:54 +0100

    Fixed Folder for HPC

--- OliverModelStochastic/src/utils/SaveData.jl
+++ OliverModelStochastic/src/utils/SaveData.jl
@@ -2,42 +2,42 @@
 
 # Save problem
 # Save 1 cargo collection
-function write_problem(problem::StowageProblem,foldername::String,filename::String)
+function write_problem(problem::StowageProblem,foldername::String,filename::String, HPC_folder::String)
     # Creates folder for results
-    if !isdir("Results/"*foldername)
-        mkdir("Results/"*foldername)
+    if !isdir("Results/"*HPC_folder*"/"*foldername)
+        mkdir("Results/"*HPC_folder*"/"*foldername)
     end
     cargo = [i for i in problem.cargo]
-    open(joinpath("Results",foldername,filename*".json"), "w") do file
+    open(joinpath("Results",HPC_folder,foldername,filename*".json"), "w") do file
         JSON.print(file, cargo, 4)  # Pretty-print with indentation
     end
     info = [problem.name,problem.timestamp]
-    open(joinpath("Results",foldername,filename*"_info"*".json"), "w") do file
+    open(joinpath("Results",HPC_folder,foldername,filename*"_info"*".json"), "w") do file
         JSON.print(file, info, 4)  # Pretty-print with indentation
     end
 end
 # Saves the cargo collection of a stochastic problem
-function write_problem_stochastic(problem::StochasticStowageProblem,foldername::String,filename::String)
+function write_problem_stochastic(problem::StochasticStowageProblem,foldername::String,filename::String,HPC_folder::String)
     # Creates folder for results
-    if !isdir("Results/"*foldername)
-        mkdir("Results/"*foldername)
+    if !isdir("Results/"*HPC_folder*"/"*foldername)
+        mkdir("Results/"*HPC_folder*"/"*foldername)
     end
     for i in 1:problem.scenarios
         cargo = [j for j in problem.cargo.items[i]]
-        open(joinpath("Results",foldername,filename*"_scenario_"*string(i)*".json"), "w") do file
+        open(joinpath("Results",HPC_folder,foldername,filename*"_scenario_"*string(i)*".json"), "w") do file
             JSON.print(file, cargo, 4)  # Pretty-print with indentation
         end
     end
     info = [problem.name,problem.unknown_weights,problem.known_weights,problem.scenarios,problem.probability,problem.timestamp]
-    open(joinpath("Results",foldername,filename*"_info"*".json"), "w") do file
+    open(joinpath("Results",HPC_folder,foldername,filename*"_info"*".json"), "w") do file
         JSON.print(file, info, 4)  
     end
 end
 # Save solutions
 # Saves deterministic solution
-function write_solution(solution::Solution,foldername::String,filename::String)
-    if !isdir("Results/"*foldername)
-        mkdir("Results/"*foldername)
+function write_solution(solution::Solution,foldername::String,filename::String,HPC_folder::String)
+    if !isdir("Results/"*HPC_folder*"/"*foldername)
+        mkdir("Results/"*HPC_folder*"/"*foldername)
     end
     placements = [cargo for cargo in solution.cargo]
     info = [solution.gap,solution.status, solution.objective, 
@@ -48,16 +48,16 @@ function write_solution(solution::Solution,foldername::String,filename::String)
         solution.tcg, solution.vcg,solution.n_variables, solution.n_binary_variables,
         solution.n_constraints, solution.model_size, solution.solver_name,
         solution.solver_iterations, solution.solver_nodes]
-    open(joinpath("Results",foldername,filename*"_placements"*".json"), "w") do file
+    open(joinpath("Results",HPC_folder,foldername,filename*"_placements"*".json"), "w") do file
         JSON.print(file, placements, 4)  # Pretty-print with indentation
     end
-    open(joinpath("Results",foldername,filename*"_info"*".json"), "w") do file
+    open(joinpath("Results",HPC_folder,foldername,filename*"_info"*".json"), "w") do file
         JSON.print(file, info, 4)  # Pretty-print with indentation
     end
 end
-function write_solution_stochastic(solution::SolutionStochastic,foldername::String,filename::String)
-    if !isdir("Results/"*foldername)
-        mkdir("Results/"*foldername)
+function write_solution_stochastic(solution::SolutionStochastic,foldername::String,filename::String,HPC_folder::String)
+    if !isdir("Results/"*HPC_folder*"/"*foldername)
+        mkdir("Results/"*HPC_folder*"/"*foldername)
     end
     placements = [cargo for cargo in solution.cargo]
     forces = [force for force in solution.forces]
@@ -68,13 +68,13 @@ function write_solution_stochastic(solution::SolutionStochastic,foldername::Stri
         solution.n_variables, solution.n_binary_variables,
         solution.n_constraints, solution.model_size, solution.solver_name,
         solution.solver_iterations, solution.solver_nodes]
-    open(joinpath("Results",foldername,filename*"_placements"*".json"), "w") do file
+    open(joinpath("Results",HPC_folder,foldername,filename*"_placements"*".json"), "w") do file
         JSON.print(file, placements, 4)  # Pretty-print with indentation
     end
-    open(joinpath("Results",foldername,filename*"_forces"*".json"), "w") do file
+    open(joinpath("Results",HPC_folder,foldername,filename*"_forces"*".json"), "w") do file
         JSON.print(file, forces, 4)
     end
-    open(joinpath("Results",foldername,filename*"_info"*".json"), "w") do file
+    open(joinpath("Results",HPC_folder,foldername,filename*"_info"*".json"), "w") do file
         JSON.print(file, info, 4)
     end
 end
@@ -82,13 +82,13 @@ end
 
 # Get data back from JSON files
 # Returns deterministic problem
-function get_deterministic_problem(foldername::String,filename::String,problemname1,problemname2,problemname3)
-    open(joinpath("Results",foldername,filename*".json"), "r") do file
+function get_deterministic_problem(foldername::String,filename::String,HPC_folder::String,problemname1,problemname2,problemname3)
+    open(joinpath("Results",HPC_folder,foldername,filename*".json"), "r") do file
         cargo = JSON3.read(read(file, String), Vector{Cargo})
         open
         # load ship data
         problem = load_data(problemname1, problemname2, problemname3)
-        open(joinpath("Results",foldername,filename*"_info"*".json"), "r") do file
+        open(joinpath("Results",HPC_folder,foldername,filename*"_info"*".json"), "r") do file
             info = JSON3.read(read(file, String), Vector{Any})
             name = info[1]
             timestamp = info[2]
@@ -104,8 +104,8 @@ function get_deterministic_problem(foldername::String,filename::String,problemna
     end
 end
 # Returns stochastic problem
-function get_stochastic_problem(foldername::String,filename::String,problemname1,problemname2,problemname3)
-    open(joinpath("Results",foldername,filename*"_info"*".json"), "r") do file
+function get_stochastic_problem(foldername::String,filename::String,HPC_folder::String,problemname1,problemname2,problemname3)
+    open(joinpath("Results",HPC_folder,foldername,filename*"_info"*".json"), "r") do file
         info = JSON3.read(read(file, String), Vector{Any})
         name = info[1]
         unknown_weights = info[2]
@@ -118,7 +118,7 @@ function get_stochastic_problem(foldername::String,filename::String,problemname1
 
         cargo = Vector{CargoCollection}()
         for i in 1:scenarios
-            open(joinpath("Results",foldername,filename*"_scenario_"*string(i)*".json"), "r") do file
+            open(joinpath("Results",HPC_folder,foldername,filename*"_scenario_"*string(i)*".json"), "r") do file
                 cargo_c = CargoCollection(JSON3.read(read(file, String), Vector{Cargo}))
                 push!(cargo, cargo_c)
             end
@@ -138,8 +138,8 @@ function get_stochastic_problem(foldername::String,filename::String,problemname1
     end
 end
 # Get deterministic solution
-function get_solution_deterministic(foldername::String,filename::String)
-    open(joinpath("Results",foldername,filename*"_info"*".json"), "r") do file
+function get_solution_deterministic(foldername::String,filename::String,HPC_folder::String)
+    open(joinpath("Results",HPC_folder,foldername,filename*"_info"*".json"), "r") do file
         info = JSON3.read(read(file, String), Vector{Any})
         gap = info[1]
         status = info[2]
@@ -172,7 +172,7 @@ function get_solution_deterministic(foldername::String,filename::String)
         solver_name = info[22]
         solver_iterations = info[23]
         solver_nodes = info[24]
-        placements = open(joinpath("Results",foldername,filename*"_placements"*".json"), "r") do file
+        placements = open(joinpath("Results",HPC_folder,foldername,filename*"_placements"*".json"), "r") do file
             JSON3.read(read(file, String), Vector{CargoPlacement})
         end
         solution = Solution(
@@ -207,8 +207,8 @@ function get_solution_deterministic(foldername::String,filename::String)
 end
 
 # Get Stochastic solution
-function get_solution_stochastic(foldername::String,filename::String)
-    open(joinpath("Results",foldername,filename*"_info"*".json"), "r") do file
+function get_solution_stochastic(foldername::String,filename::String,HPC_folder::String)
+    open(joinpath("Results",HPC_folder,foldername,filename*"_info"*".json"), "r") do file
         info = JSON3.read(read(file, String), Vector{Any})
         gap = info[1]
         status = info[2]
@@ -233,10 +233,10 @@ function get_solution_stochastic(foldername::String,filename::String)
         solver_name = info[14]
         solver_iterations = info[15]
         solver_nodes = info[16]
-        placements = open(joinpath("Results",foldername,filename*"_placements"*".json"), "r") do file
+        placements = open(joinpath("Results",HPC_folder,foldername,filename*"_placements"*".json"), "r") do file
             JSON3.read(read(file, String), Vector{CargoPlacementStochastic})
         end
-        forces = open(joinpath("Results",foldername,filename*"_forces"*".json"), "r") do file
+        forces = open(joinpath("Results",HPC_folder,foldername,filename*"_forces"*".json"), "r") do file
             JSON3.read(read(file, String), Vector{WeightAndForceStochastic})
         end
 
@@ -270,7 +270,7 @@ function HPC_data(repetitions::Int64, scenarios::Vector, n_unknown::Vector,HPC_f
     if !isdir("Results/"*HPC_folder)
         mkdir("Results/"*HPC_folder)
     end
-    d = Dat = Dates.format(now(), "dd/mm_HH:MM:SS")
+    d = Dates.format(now(), "dd/mm_HH:MM:SS")
     filename = "HPC_test"*d
     open(joinpath("Results",HPC_folder,filename*".json"), "w") do file
         JSON.print(file, data, 4)  # Pretty-print with indentation
