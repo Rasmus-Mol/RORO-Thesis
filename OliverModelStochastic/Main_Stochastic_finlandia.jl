dc3b2acd3b264f701abe04e54954af99166b4eed
mollergøj <s194364@student.dtu.dk>
2025-05-14 20:33:24 +0200

    last changes before tests, v4

--- OliverModelStochastic/Main_Stochastic_finlandia.jl
+++ OliverModelStochastic/Main_Stochastic_finlandia.jl
@@ -6,8 +6,8 @@ include("src/utils/test_instances.jl")
 using .StowagePlannerStochastic
 using JuMP
 
-parse_index = parse(Int, ARGS[1]) # Jobindex input
-
+#parse_index = parse(Int, ARGS[1]) # Jobindex input
+parse_index = 1
 # Choose instance:
 test_problem_name = Finlandia_test[5]
 
@@ -23,7 +23,7 @@ if parse_index == 1
     # Creates Deterministic problem and model
     model_det = create_model(problem_det)
     set_silent(model_det) # removes terminal output
-    set_time_limit_sec(model_det, 60 * 60) # 1 hour
+    set_time_limit_sec(model_det, 60 * 5) # 1 hour
     optimize!(model_det)
     solution_det = extract_solution(problem_det, model_det)
     # Save Solution for deterministic problem
@@ -37,8 +37,8 @@ end
 scenarios = [10,20,30,40,50]
 sc = scenarios[parse_index] # number scenarios for current job
 n_cargo_unknownweight = [length(problem_det.cargo)] # all cargo weights are unknown
-time_limit = 60 * 60 # 1 hour
-repetitions = 1 # number of repetitions of same inputs
+time_limit = 60 * 5 # 1 hour
+repetitions = 2 # number of repetitions of same inputs
 
 # Check if folder and file has been created, otherwise create
 file_check = "Results/"*HPC_folder*"HPC_data.json"
