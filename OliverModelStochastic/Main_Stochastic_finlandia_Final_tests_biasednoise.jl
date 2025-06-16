a7a8e6e62679856e1f7934b71d3d6d433cb50d9d
mollergøj <s194364@student.dtu.dk>
2025-06-16 18:30:46 +0200

    code to run tests on hpc

--- OliverModelStochastic/Main_Stochastic_finlandia_Final_tests_biasednoise.jl
+++ OliverModelStochastic/Main_Stochastic_finlandia_Final_tests_biasednoise.jl
@@ -19,11 +19,12 @@ problem_det = load_data("finlandia", test_problem_name, "hazardous")
 # Should be decided more precisely at some point
 initial_scenarios = 100
 # Folder name for results - date and hour
-HPC_folder = "Finlandia_"*test_problem_name*"_Final_Tests_init_scenarios_$(initial_scenarios)_biasednoise_"*Dates.format(now(), "dd_mm_HH")
- # Describe tests if necessary
-extra_info = "Ship: Finlandia, Test problem: "*test_problem_name*" - Yes Scenario reduction, Yes noise, Yes EVP"
+HPC_folder = "Finlandia_" * test_problem_name * "_sc_$(initial_scenarios)_biasednoise_" * Dates.format(now(), "dd_mm_HH")
+# Describe tests if necessary
+extra_info = "Ship: Finlandia, Test problem: " * test_problem_name * " - Yes Scenario reduction, Yes noise, Yes EVP"
 
 # First job index - create problem 
+#=
 if parse_index == 1
     # Creates Deterministic problem and model
     model_det = create_model(problem_det)
@@ -34,6 +35,7 @@ if parse_index == 1
     # Save Solution for deterministic problem
     write_solution(solution_det,"Finlandia_deterministic","Deterministic_Solution",HPC_folder)
 end
+=#
 
 # Scenarios we test - length has to fit to
 # TODO: Should change when I know how many is needed
@@ -44,101 +46,39 @@ sc = scenarios[1]
 #sc = scenarios[parse_index] # number scenarios for current job
 n_cargo_unknownweight = [length(problem_det.cargo)] # all cargo weights are unknown
 time_limit = 60 * 60 # 1 hour
-repetitions = 10 # number of repetitions of same inputs
+repetitions = 5 # number of repetitions of same inputs
 
 # Check if folder and file has been created, otherwise create
-file_check = "Results/"*HPC_folder*"HPC_data.json"
+file_check = "Results/" * HPC_folder * "HPC_data.json"
 if !isfile(file_check)
     # Save scenario and number of unknown weights
     write_HPC_data(repetitions, scenarios, n_cargo_unknownweight, time_limit, HPC_folder, extra_info)
 end
 
+obj_val_EVP = Array{Any}(nothing, repetitions,sc)
+cargo_load_EVP = Array{Any}(nothing, repetitions,sc)
+ballast_used_EVP = Array{Any}(nothing, repetitions,sc)
+inf_index_EVP = zeros(repetitions, sc)
+
 # Run tests
 for i in 1:repetitions
-    #=
-    # uniform random sampling method
-    # not necessary to do scenario reduction on this method
-    pro = create_stochastic_problem(problem_det, initial_scenarios, n_cargo_unknownweight[1], []) 
-    # Save problem
-    foldername = "Stochastic_random_sampling_rep$(i)_sc$(sc)_unknown$(n_cargo_unknownweight[1])_time$(time_limit)"
-    write_problem_stochastic(pro,foldername,"Stochastic_Problem",HPC_folder)
-    mo = create_model_stochastic(pro)
-    set_silent(mo) # removes terminal output
-    set_time_limit_sec(mo, time_limit)
-    optimize!(mo)
-    sol = extract_stochastic_solution(pro,mo)
-    # Save solution
-    write_solution_stochastic(sol,foldername,"Stochastic_Solution",HPC_folder)
-    cs_sol = sol.cs
-    # Solve second stage when we know unknown weights
-    second_stage_m = second_stage_model(cs_sol, problem_det)
-    set_silent(second_stage_m) # removes terminal output
-    set_time_limit_sec(second_stage_m, time_limit) 
-    optimize!(second_stage_m)
-    fitted_sol = get_solution_second_stage_stochastic(problem_det, second_stage_m, sol)
-    # Save fitted solution
-    write_solution(fitted_sol,foldername,"Fitted_Solution",HPC_folder)
-    # Check if second-stage problem was feasible
-    #println(fitted_sol.status)
-    #if fitted_sol.status == "INFEASIBLE"
-    if fitted_sol.status ∈ [MOI.INFEASIBLE, MOI.INFEASIBLE_OR_UNBOUNDED]
-        second_stage_m_slacked = second_stage_model_slack(cs_sol, problem_det)
-        set_silent(second_stage_m_slacked) # removes terminal output
-        set_time_limit_sec(second_stage_m_slacked, time_limit) 
-        optimize!(second_stage_m_slacked)
-        fitted_sol_slacked = get_solution_second_stage_stochastic(problem_det, second_stage_m_slacked, sol)
-        write_solution(fitted_sol_slacked,foldername,"Fitted_Solution_slacked",HPC_folder)
-        write_slack(HPC_folder, foldername, "Fitted_Solution", second_stage_m_slacked)
-    end
-    # EVP method for uniform random sampling method 
-    foldername = "EVP_random_sampling_rep$(i)_sc$(sc)_unknown$(n_cargo_unknownweight[1])_time$(time_limit)"
-    pro = expected_value_problem(pro)
-    # Save problem
-    write_problem(pro,foldername,"EVP_Problem",HPC_folder)
-    mo = create_model(pro)
-    set_silent(mo) # removes terminal output
-    set_time_limit_sec(mo, time_limit)
-    optimize!(mo)
-    sol = extract_solution(pro, mo)
-    # Save solution
-    write_solution(sol,foldername,"EVP_Solution",HPC_folder)
-    cs_sol = sol.cs
-    second_stage_m = second_stage_model(cs_sol, problem_det)
-    set_silent(second_stage_m) # removes terminal output
-    set_time_limit_sec(second_stage_m, time_limit) 
-    optimize!(second_stage_m)
-    fitted_sol = get_solution_second_stage_deterministic(problem_det, second_stage_m, sol)
-    # Save fitted solution
-    write_solution(fitted_sol,foldername,"Fitted_Solution",HPC_folder)
-    # Check if second-stage problem was feasible
-    #fitted_sol.status == "INFEASIBLE"
-    if fitted_sol.status ∈ [MOI.INFEASIBLE, MOI.INFEASIBLE_OR_UNBOUNDED]
-        second_stage_m_slacked = second_stage_model_slack(cs_sol, problem_det)
-        set_silent(second_stage_m_slacked) # removes terminal output
-        set_time_limit_sec(second_stage_m_slacked, time_limit) 
-        optimize!(second_stage_m_slacked)
-        fitted_sol_slacked = get_solution_second_stage_deterministic(problem_det, second_stage_m_slacked, sol)
-        write_solution(fitted_sol_slacked,foldername,"Fitted_Solution_slacked",HPC_folder)
-        write_slack(HPC_folder, foldername, "Fitted_Solution", second_stage_m_slacked)
-    end
-    =#
     # Bootstrap method 1
     # TODO: scenario reduction
     #problem_det_noise = add_white_noise_to_test_instance(problem_det)
     problem_det_noise = add_biased_noise_to_test_instance(problem_det)
-    pro = create_stochastic_problem(problem_det_noise, initial_scenarios, n_cargo_unknownweight[1], [],Bootstrap_bookedweight_quantile) 
+    pro = create_stochastic_problem(problem_det_noise, initial_scenarios, n_cargo_unknownweight[1], [], Bootstrap_bookedweight_quantile)
     # TODO: how many scenarios reduce to, i.e. sc
-    pro = scenario_reduced(pro, sc, scenario_reduction_clustering,60)
+    pro = scenario_reduced(pro, sc, scenario_reduction_clustering, 60)
     # Save problem
     foldername = "Stochastic_Bootstrap1_rep$(i)_sc$(sc)_unknown$(n_cargo_unknownweight[1])_time$(time_limit)"
-    write_problem_stochastic(pro,foldername,"Stochastic_Problem",HPC_folder)
+    #write_problem_stochastic(pro,foldername,"Stochastic_Problem",HPC_folder)
     mo = create_model_stochastic(pro)
     set_silent(mo) # removes terminal output
     set_time_limit_sec(mo, time_limit)
     optimize!(mo)
-    sol = extract_stochastic_solution(pro,mo)
+    sol = extract_stochastic_solution(pro, mo)
     # Save solution
-    write_solution_stochastic(sol,foldername,"Stochastic_Solution",HPC_folder)
+    write_solution_stochastic(sol, foldername, "Stochastic_Solution", HPC_folder)
     cs_sol = sol.cs
     # Solve second stage when we know unknown weights
     second_stage_m = second_stage_model(cs_sol, problem_det)
@@ -147,71 +87,97 @@ for i in 1:repetitions
     optimize!(second_stage_m)
     fitted_sol = get_solution_second_stage_stochastic(problem_det, second_stage_m, sol)
     # Save fitted solution
-    write_solution(fitted_sol,foldername,"Fitted_Solution",HPC_folder)
+    write_solution(fitted_sol, foldername, "Fitted_Solution", HPC_folder)
     # Check if second-stage problem was feasible
     #if fitted_sol.status == "INFEASIBLE"
     if fitted_sol.status ∈ [MOI.INFEASIBLE, MOI.INFEASIBLE_OR_UNBOUNDED]
         second_stage_m_slacked = second_stage_model_slack(cs_sol, problem_det)
         set_silent(second_stage_m_slacked) # removes terminal output
-        set_time_limit_sec(second_stage_m_slacked, time_limit) 
+        set_time_limit_sec(second_stage_m_slacked, time_limit)
         optimize!(second_stage_m_slacked)
         fitted_sol_slacked = get_solution_second_stage_stochastic(problem_det, second_stage_m_slacked, sol)
-        write_solution(fitted_sol_slacked,foldername,"Fitted_Solution_slacked",HPC_folder)
+        write_solution(fitted_sol_slacked, foldername, "Fitted_Solution_slacked", HPC_folder)
         if primal_status(second_stage_m_slacked) == MOI.FEASIBLE_POINT
             write_slack(HPC_folder, foldername, "Fitted_Solution", second_stage_m_slacked)
-        end 
+        end
     end
     # EVP method for Bootstrap method 1
     foldername = "EVP_Bootstrap1_rep$(i)_sc$(sc)_unknown$(n_cargo_unknownweight[1])_time$(time_limit)"
     sto_pro = pro
     pro = expected_value_problem(pro)
     # Save problem
-    write_problem(pro,foldername,"EVP_Problem",HPC_folder)
+    write_problem(pro, foldername, "EVP_Problem", HPC_folder)
     mo = create_model(pro)
     set_silent(mo) # removes terminal output
     set_time_limit_sec(mo, time_limit)
     optimize!(mo)
     sol = extract_solution(pro, mo)
     # Save solution
-    write_solution(sol,foldername,"EVP_Solution",HPC_folder)
+    write_solution(sol, foldername, "EVP_Solution", HPC_folder)
     cs_sol = sol.cs
     # Finding solutions to each scenario for EVP
-    for l in 1:sc
+    for h in 1:sc
         # create problems
         pro_temp = StowageProblem(
-            vessel = problem_det.vessel,
-            slots = problem_det.slots,
-            cargo = pro_sto.cargo.items[l],    
+            vessel=problem_det.vessel,
+            slots=problem_det.slots,
+            cargo=sto_pro.cargo.items[h],
             # Problem metadata
-            name = "EVP Bootstrap $(l)",
-            timestamp = now()
+            name="EVP Random $(h)",
+            timestamp=now()
         )
-        EVP_model = second_stage_model(cs_sol, pro_temp)
+        # Just using slack model initially
+        EVP_model = second_stage_model_slack(cs_sol, pro_temp)
         set_silent(EVP_model) # removes terminal output
         set_time_limit_sec(EVP_model, time_limit)
         optimize!(EVP_model)
         fitted_sol_EVP = get_solution_second_stage_deterministic(pro_temp, EVP_model, sol)
-        write_solution(fitted_sol_EVP, foldername, "Fitted_Solution_EVP_$(l)", HPC_folder)
-    end
-    second_stage_m = second_stage_model(cs_sol, problem_det)
-    set_silent(second_stage_m) # removes terminal output
-    set_time_limit_sec(second_stage_m, time_limit) # 5 minutes to solve model
-    optimize!(second_stage_m)
-    fitted_sol = get_solution_second_stage_deterministic(problem_det, second_stage_m, sol)
-    # Save fitted solution
-    write_solution(fitted_sol,foldername,"Fitted_Solution",HPC_folder)
-    # Check if second-stage problem was feasible
-    #fitted_sol.status == "INFEASIBLE"
-    if fitted_sol.status ∈ [MOI.INFEASIBLE, MOI.INFEASIBLE_OR_UNBOUNDED]
-        second_stage_m_slacked = second_stage_model_slack(cs_sol, problem_det)
-        set_silent(second_stage_m_slacked) # removes terminal output
-        set_time_limit_sec(second_stage_m_slacked, time_limit) 
-        optimize!(second_stage_m_slacked)
-        fitted_sol_slacked = get_solution_second_stage_deterministic(problem_det, second_stage_m_slacked, sol)
-        write_solution(fitted_sol_slacked,foldername,"Fitted_Solution_slacked",HPC_folder)
-        if primal_status(second_stage_m_slacked) == MOI.FEASIBLE_POINT
-            write_slack(HPC_folder, foldername, "Fitted_Solution", second_stage_m_slacked)
-        end 
+        slack = [value.(EVP_model[:slack_deck]), value.(EVP_model[:slack_Vmax]),value.(EVP_model[:slack_Vmin]),
+            value.(EVP_model[:slack_Tmin]), value.(EVP_model[:slack_Tmax]), 
+            value.(EVP_model[:slack_Lmin]), value.(EVP_model[:slack_Lmax]),
+            #value.(model[:slack_shear1]), value.(model[:slack_shear2]), 
+            value.(EVP_model[:slack_shearMin]),
+            value.(EVP_model[:slack_shearMax]), value.(EVP_model[:slack_bendingMax]),
+             value.(EVP_model[:slack_ballast_tanks])
+            ] 
+        # Check if slack is used
+        if sum(slack) > 0.001
+            inf_index_EVP[i,h] = 1
+        end
+        obj_val_EVP[i,h] = fitted_sol_EVP.objective
+        cargo_load_EVP[i,h] = fitted_sol_EVP.n_cargo_loaded
+        ballast_used_EVP[i,h] = fitted_sol_EVP.ballast_weight
+        #write_solution(fitted_sol_EVP, foldername, "Fitted_Solution_EVP_$(h)", HPC_folder)
+        #if primal_status(EVP_model) == MOI.FEASIBLE_POINT || termination_status(EVP_model) == MOI.OPTIMAL
+        #    write_slack(HPC_folder, foldername, "Fitted_Solution_$(h)", EVP_model)
+        #end
     end
 end
+# Save results
+Save_EVP_results(HPC_folder, "Objective_values", obj_val_EVP)
+Save_EVP_results(HPC_folder, "Cargo_loaded", cargo_load_EVP)
+Save_EVP_results(HPC_folder, "Ballast_used", ballast_used_EVP)
+Save_EVP_results(HPC_folder, "Inf_index", inf_index_EVP)
 
+# Function should be copied to savedata.jl
+function Save_EVP_results_2D(HPC_folder::String,filename::String ,matrix)
+    if !isdir("Results/"*HPC_folder)
+        mkdir("Results/"*HPC_folder)
+    end
+    nested = [[matrix[i, j] for j in 1:size(matrix, 2)] for i in 1:size(matrix, 1)]
+    open(joinpath("Results",HPC_folder,filename*".json"), "w") do file
+        JSON.print(file, nested, 4)
+    end
+end
+function Get_EVP_results_2D(HPC_folder::String, filename::String)
+    temp = open(joinpath("Results",HPC_folder,filename*".json"), "r") do file
+        JSON3.read(read(file, String))#, Vector{Vector{Float64}})
+    end
+    rows = length(temp)
+    cols = length(temp[1])
+    mat = Array{Any}(undef, rows, cols)
+    for i in 1:rows, j in 1:cols
+        mat[i, j] = temp[i][j]
+    end
+    return mat
+end
--- OliverModelStochastic/Results_ScenarioReduction.jl
+++ OliverModelStochastic/Results_ScenarioReduction.jl
@@ -1,27 +1,33 @@
 # Estimate effect of scenario reduction
 include("packages_and_files.jl")
+plot_folder = "Plots/Results/BiasedNoise"
+# Create folder for plots
+if !isdir(plot_folder)
+    mkpath(plot_folder)
+end
 
 Deterministic_problem = Array{Any}(undef, 8)
 problemname1, problemname3 = "finlandia", "hazardous"
 for i in 1:8
     Deterministic_problem[i] = load_data(problemname1, Finlandia_test[i], problemname3)
 end
-
 scenarios = [10]
 sc = scenarios[1]
 scenario_reduction = [100,300,500]
 sto_no_reduction = Array{Any}(undef, 8, length(scenario_reduction))
 sto_reduction = Array{Any}(undef, 8, length(scenario_reduction))
+pro_reduction = Array{Any}(undef, 8, length(scenario_reduction))
 # add biased noise
 # Create problem using few scenarios and no scenario reduction
 # Create problem using many scenarios and using scenario reduction
 # Estimate which is closest to Deterministic problem ?
 for i in 1:8 # number of instances
+    println("it: ", i)
     problem_det_noise = add_biased_noise_to_test_instance(Deterministic_problem[i])
     sto_no_reduction[i] = create_stochastic_problem(problem_det_noise, sc, length(Deterministic_problem[i].cargo), [], Bootstrap_bookedweight_quantile)  
     for j in 1:length(scenario_reduction)
-        pro = create_stochastic_problem(problem_det_noise, scenario_reduction[j], length(Deterministic_problem[i].cargo), [], Bootstrap_bookedweight_quantile)
-        sto_reduction[i,j] = scenario_reduced(pro, sc, scenario_reduction_clustering, 60)
+        pro_reduction[i,j] = create_stochastic_problem(problem_det_noise, scenario_reduction[j], length(Deterministic_problem[i].cargo), [], Bootstrap_bookedweight_quantile)
+        sto_reduction[i,j] = scenario_reduced(pro_reduction[i,j] , sc, scenario_reduction_clustering, 60)
     end
 end
 
@@ -48,57 +54,134 @@ end
 
 avg_dist = [mean(dist_no_reduction[i]) for i in 1:8]
 avg_dist_reduction = [mean(dist_reduction[i,j]) for i in 1:8, j in 1:length(scenario_reduction)]
-p = plot(avg_dist, label = "No reduction", title = "Average distance from original problem", xlabel = "Instance", ylabel = "Distance")
+p = plot(avg_dist, label = "No reduction", title = "Average distance to deterministic instance weight", xlabel = "Instance", ylabel = "Distance")
 for j in 1:length(scenario_reduction)
     plot!(p, avg_dist_reduction[:,j], label = "Initialscenarios $(scenario_reduction[j])")
 end
 display(p)
+savefig(p, plot_folder*"/Average_distance_to_deterministic_instance_weight.png")
 # distance to original problem
 n = length.(dist_no_reduction)   # number of samples per instance
 means = mean.(dist_no_reduction)
 stds = std.(dist_no_reduction)
-stderr = stds ./ sqrt.(n)
+stderrs = stds ./ sqrt.(n)
 # 95% confidence interval z-value
 z = 1.96
-lower = means .- z .* stderr
-upper = means .+ z .* stderr
-yerr = z .* stderr
+lower = means .- z .* stderrs
+upper = means .+ z .* stderrs
+yerr = z .* stderrs
 # Plot with error bars
-plot(
+p1 = plot(
     means,
     yerror = yerr,
     #ribbon = (means .- lower, upper .- means),
     xlabel = "Instance",
-    ylabel = "Mean distance from original problem",
+    ylabel = "Mean distance",
     title = "No scenario reduction",
     label = ""
     #label = "Mean ± CI",
     #legend = :topright
 )
+savefig(p1, plot_folder*"/Mean_distance_No_scenario_reduction.png")
 for i in 1:length(scenario_reduction)
     n = length.(dist_reduction[:,i])   # number of samples per instance
     means = mean.(dist_reduction[:,i])
     stds = std.(dist_reduction[:,i])
-    stderr = stds ./ sqrt.(n)
+    stderrs = stds ./ sqrt.(n)
     # 95% confidence interval z-value
     z = 1.96
-    lower = means .- z .* stderr
-    upper = means .+ z .* stderr
-    yerr = z .* stderr
+    lower = means .- z .* stderrs
+    upper = means .+ z .* stderrs
+    yerr = z .* stderrs
     # Plot with error bars
     p = plot(
         means,
         yerror = yerr,
         #ribbon = (means .- lower, upper .- means),
         xlabel = "Instance",
-        ylabel = "Mean distance from original problem",
+        ylabel = "Mean distance",
         title = "Scenario reduction - initial scenarios $(scenario_reduction[i])",
         label = ""
         #label = "Mean ± CI",
         #legend = :topright
     )
     display(p)
+    savefig(p, plot_folder*"/Mean_distance_scenario_reduction_sc_$(scenario_reduction[i]).png")
 end
+# Find silhouette scores
+# for 1 instance
+temp = silhouette_score(pro_reduction[1,1].cargo ,10)
+clusters = [5,10,15]
+s_means = zeros(length(clusters),8, length(scenario_reduction))
+for i in 1:8
+    println("Instance: ", i)
+    for j in 1:length(scenario_reduction)
+        for l in 1:length(clusters) # different number of clusters
+            s_means[l,i,j] = mean(silhouette_score(pro_reduction[i,j].cargo ,clusters[l]))
+        end
+    end
+end
+for i in 1:8
+    println("##############")
+    println("instance: ", i)
+    println(round.(s_means[2,i,:], digits=4))
+end
+s_means_instances = round.(mean(s_means, dims = 2), digits = 4)
+println(s_means_instances[:,1,:])
+s_means[end,:,:]
+CargoC = pro_reduction[5,1].cargo
+sc = length(CargoC.items)
+cost = zeros(sc, sc)
+for i in 1:sc
+    for j in 1:sc
+        cost[i,j] = sum(abs.(getfield.(CargoC.items[i],:weight) .- getfield.(CargoC.items[j],:weight)))
+    end
+end
+# Perform hierarchical clustering
+result = hclust(cost, linkage = :complete) #:average)  # or :single, :complete, :ward, etc.
+# Cut the dendrogram into sc clusters
+labels = cutree(result, k = 10)
+scores = silhouette_score(pro_reduction[5,1].cargo ,10)
+# plot silhouette scores
+plot_silhouette(scores, labels)
+plot_silhouette2(scores, labels)
+mean(scores)
+scores
+
+
+###############
+sol_evp = Array{Any}(nothing, 5,6,7,8)
+nested = [[[ [sol_evp[1,j,k,l] for l in 1:size(sol_evp,4)]
+for k in 1:size(sol_evp,3)]
+for j in 1:size(sol_evp,2)] for i in 1:size(sol_evp,1)]
+
+open("test_EVP.json", "w") do file
+    JSON.print(file, nested, 4)
+end
+function get_obj_of_evp(filename::String)
+    open(filename*".json", "r") do file
+        obj = JSON3.read(read(file, String))#, Vector{Vector{Vector{Vector{Float64}}}})
+        return obj
+    end
+end
+as = get_obj_of_evp("test_EVP")
+ass = to_4d_array(as)
+
+length(as)
+function to_4d_array(nested)
+    dims = (
+        length(nested),
+        length(nested[1]),
+        length(nested[1][1]),
+        length(nested[1][1][1])
+    )
+    A = Array{Any, 4}(undef, dims...)
+    for i in 1:dims[1], j in 1:dims[2], k in 1:dims[3], l in 1:dims[4]
+        A[i, j, k, l] = nested[i][j][k][l]
+    end
+    return A
+end
+###############
 
 function distance_from_original_problem(sto_pro::StochasticStowageProblem, det_pro::StowageProblem)
     # Calculate distance between the cargoes in the problem and the original problem
@@ -111,4 +194,107 @@ function distance_from_original_problem(sto_pro::StochasticStowageProblem, det_p
         dist[i] = round(sum(abs.(getfield.(sto_truckssecu,:weight) .- getfield.(det_truckssecu,:weight))),digits = 2)
     end
     return dist
+end
+
+function silhouette_score(CargoC::CargoCollectionScenarios, no_clusters)
+    sc = length(CargoC.items)
+    cost = zeros(sc, sc)
+    for i in 1:sc
+        for j in 1:sc
+            cost[i,j] = sum(abs.(getfield.(CargoC.items[i],:weight) .- getfield.(CargoC.items[j],:weight)))
+        end
+    end
+    # Perform hierarchical clustering
+    result = hclust(cost, linkage = :complete) #:average)  # or :single, :complete, :ward, etc.
+    # Cut the dendrogram into sc clusters
+    labels = cutree(result, k = no_clusters)
+    # Find silhouette scores
+    n = size(cost, 1)
+    s = zeros(n)
+
+    for i in 1:n
+        same_cluster = findall(j -> labels[j] == labels[i] && j != i, 1:n)
+        other_clusters = setdiff(unique(labels), [labels[i]])
+
+        # a(i): avg distance to same cluster
+        a_i = isempty(same_cluster) ? 0.0 : mean(cost[i, same_cluster])
+
+        # b(i): min avg distance to other clusters
+        b_i = minimum([
+            mean(cost[i, findall(j -> labels[j] == c, 1:n)])
+            for c in other_clusters
+        ])
+
+        s[i] = if a_i == b_i
+            0.0
+        elseif a_i < b_i
+            1 - a_i / b_i
+        else
+            b_i / a_i - 1
+        end
+        if a_i == 0.0
+            s[i] = 0
+        end
+    end
+    return s
+end
+
+function plot_silhouette(scores::Vector{Float64}, labels::Vector{Int})
+    n = length(scores)
+    cluster_ids = sort(unique(labels))
+    cluster_scores = [scores[findall(labels .== c)] for c in cluster_ids]
+    
+    # Sort each cluster's scores (optional, for prettier plot)
+    sorted_scores = [sort(s, rev=true) for s in cluster_scores]
+
+    # Prepare bar data
+    y = Float64[]
+    cluster_colors = Int[]
+    x = Int[]
+    tick_positions = Int[]
+    tick_labels = String[]
+    pos = 1
+
+    for (i, scs) in enumerate(sorted_scores)
+        append!(y, scs)
+        append!(cluster_colors, fill(i, length(scs)))
+        append!(x, pos:pos+length(scs)-1)
+        push!(tick_positions, pos + length(scs) ÷ 2)
+        push!(tick_labels, "Cluster $(cluster_ids[i])")
+        pos += length(scs)
+    end
+
+    bar(x, y, group=cluster_colors, legend=false, xlabel="Data points", ylabel="Silhouette Score",
+        title="Silhouette Scores by Cluster", xticks=(tick_positions, tick_labels))
+end
+
+function plot_silhouette2(scores::Vector{Float64}, labels::Vector{Int})
+    n = length(scores)
+    cluster_ids = sort(unique(labels))
+    cluster_scores = [scores[findall(labels .== c)] for c in cluster_ids]
+    
+    # Sort scores within each cluster (optional for aesthetics)
+    sorted_scores = [sort(s, rev=true) for s in cluster_scores]
+
+    y = Float64[]
+    cluster_colors = Int[]
+    x = Int[]
+    tick_positions = Int[]
+    tick_labels = String[]
+    pos = 1
+
+    for (i, scs) in enumerate(sorted_scores)
+        append!(y, scs)
+        append!(cluster_colors, fill(i, length(scs)))
+        append!(x, pos:pos+length(scs)-1)
+        push!(tick_positions, pos + length(scs) ÷ 2)
+        push!(tick_labels, "Cluster $(cluster_ids[i])")
+        pos += length(scs)
+    end
+
+    bar(x, y, group=cluster_colors, legend=false,
+        xlabel="Data Points", ylabel="Silhouette Score",
+        title="Silhouette Scores by Cluster",
+        xticks=(tick_positions, tick_labels),
+        xrotation=45)  # << Rotate x-axis labels by 45 degrees
 end
